ba4d2371bf4c607f308338eb6b70d11dce0b182c
Fikri Yustiawan <fikriyustiawan13@gmail.com>
2026-09-01 19:16:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+nilai = int(input("Masukkan nilai matematika: "))
+
+if nilai < 75:
+    print("Siswa harus mengikuti ujian remedial")
+else:
+    print("Siswa tidak perlu mengikuti ujian remedial")
